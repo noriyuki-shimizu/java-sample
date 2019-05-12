794053849c95b543e250c08cb85c0c4155fc1b64
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-05-13 01:04:04 +0900

    インスタンスの不備を修正

--- src/com/sample3/Main.java
+++ src/com/sample3/Main.java
@@ -1,6 +1,7 @@
 package com.sample3;
 
 import com.sample3.domain.foods.*;
+import com.sample3.domain.foods.list.FoodList;
 import com.sample3.usecase.intaractor.Command;
 import com.sample3.usecase.ICommand;
 
@@ -8,7 +9,7 @@ import java.util.Arrays;
 
 public class Main {
 
-    private static ICommand command = new Command(Arrays.asList(
+    private static FoodList foodList = new FoodList(Arrays.asList(
             new Western("ハンバーグカレードリア", 799),
             new Western("ベーコンチーズハンバーグ", 999),
             new Western("3種のエビのドリア", 899),
@@ -16,6 +17,8 @@ public class Main {
             new Drink("ドリンクバー", 399),
             new Junk("ポテト", 299)
     ));
+
+    private static ICommand command = new Command(foodList);
 
     public static void main(String[] args) {
         command.displayMenuList();
