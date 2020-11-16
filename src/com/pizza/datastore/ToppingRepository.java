27dbccb4966953d4c430f03d84502fd7f9133e52
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-11-16 20:00:21 +0900

    トッピングの repository を追加

---
+++ src/com/pizza/datastore/ToppingRepository.java
@@ -0,0 +1,7 @@
+package com.pizza.datastore;
+
+import com.pizza.domain.entity.Topping;
+import com.pizza.domain.repository.AbstractRepository;
+
+public class ToppingRepository extends AbstractRepository<Topping, String> {
+}
