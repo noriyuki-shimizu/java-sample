28656e7484c99a9da5b2d409bde4d4a82602a9cd
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-11-29 16:18:15 +0900

    ID を自動採番するよう修正

--- src/com/pizza/domain/entity/Topping.java
+++ src/com/pizza/domain/entity/Topping.java
@@ -1,10 +1,16 @@
 package com.pizza.domain.entity;
 
-public class Topping extends Entity {
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Topping extends Entity<String> {
+    private static final AtomicLong ORDER_INDEX = new AtomicLong();
+
     private final String name;
     private final int money;
 
     private Topping(final String name, final int money) {
+        super(UUID.randomUUID().toString(), ORDER_INDEX.incrementAndGet());
         this.name = name;
         this.money = money;
     }
@@ -23,6 +29,6 @@ public class Topping extends Entity {
 
     @Override
     public String toString() {
-        return String.format("topping: { id=%s, name=%s, money=%4d }", super.id, name, money);
+        return String.format("topping: { id=%s, order=%2d, name=%s, money=%4d }", super.id, super.order, name, money);
     }
 }
